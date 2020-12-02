855e96154beca5e05b37229fb78acab99d25f01f
epixieme <39728053+epixieme@users.noreply.github.com>
2020-12-02 16:42:19 +0000

    Delete const deposit.js

--- const deposit.js
+++
@@ -1,8 +0,0 @@
-const deposit = 100;
-const rate = 20;
-const threshold = 170;
-const depositProfit=(deposit, rate, threshold)=>Math.ceil(Math.log(threshold/deposit)/Math.log(Math.pow(rate/100/1 +1, 1)))
-
-depositProfit(deposit, rate, threshold)
-
-///challenge
