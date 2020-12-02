79376d7d6ff7ec9516d81e792a80ed97370537d9
Kirstie Hayes <kirstiehayes@kirsties-mbp.home>
2020-12-02 16:21:12 +0000

    updating code

--- constdeposit.js
+++ constdeposit.js
@@ -1,3 +1,19 @@
+
+//steps to work it out
+// let amount = Math.log(threshold/deposit)
+
+// let interest=rate/100
+
+// let periodicRate =interest/1 +1
+
+// let raiseBase = Math.pow(periodicRate, 1)
+
+// let log = Math.log(raiseBase)
+
+// let result = Math.ceil(amount/log)
+
+// return result.
+
 const deposit = 100;
 const rate = 20;
 const threshold = 170;
@@ -5,4 +21,3 @@ const depositProfit=(deposit, rate, threshold)=>Math.ceil(Math.log(threshold/dep
 
 depositProfit(deposit, rate, threshold)
 
-///challenges
