2d2e47366a6f34d7be5d74abcfa5ab4e65a774aa
Kirstie Hayes <kirstiehayes@kirsties-mbp.home>
2020-12-02 15:20:20 +0000

    deposit challenge

--- const deposit.js
+++ const deposit.js
@@ -4,3 +4,5 @@ const threshold = 170;
 const depositProfit=(deposit, rate, threshold)=>Math.ceil(Math.log(threshold/deposit)/Math.log(Math.pow(rate/100/1 +1, 1)))
 
 depositProfit(deposit, rate, threshold)
+
+///challenge
